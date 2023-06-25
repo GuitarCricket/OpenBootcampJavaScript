8a23c75df24424ed76666f05c5785569e4427d19
GuitarCricket <guitarcricket@gmail.com>
2023-06-25 07:10:06 -0400

    Ejercicios de numeros en JavaScript

---
+++ 5-numeros-javascript/ejercicio-numeros-javascript.js
@@ -0,0 +1,18 @@
+
+altura = 177;
+
+alturaM = 1.77;
+
+peso = 81.5;
+
+alturaRedondeada = Math.ceil(alturaM)
+console.log(alturaRedondeada)
+
+pesoRedondeadoAbajo =  Math.floor(peso)
+console.log(pesoRedondeadoAbajo)
+
+const maximoValor = Number.MAX_VALUE;
+const maximoValorMasUno = maximoValor + 1;
+
+const sonIguales = maximoValorMasUno === maximoValor;
+console.log(sonIguales);
