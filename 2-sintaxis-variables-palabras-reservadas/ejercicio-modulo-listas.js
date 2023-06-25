9d5e9b0b586eec9e25942eb2f75c3a15247d1de7
GuitarCricket <guitarcricket@gmail.com>
2023-06-24 20:08:00 -0400

    Ejercicio modulo 2

---
+++ 2-sintaxis-variables-palabras-reservadas/ejercicio-modulo-listas.js
@@ -0,0 +1,15 @@
+
+const miCumple = new Date('1988-06-02')
+console.log(miCumple)
+
+const miLibroFavorito = {
+    titulo: 'Danhauser-Teoria de la musica',
+    fecha: 1872,
+    autor: 'Adolphe Danhauser',
+    paginas: 123,
+    url: 'http://www.el-atril.com/partituras/Danhauser/danhauser%20-%20teoria%20de%20la%20musica.pdf'
+}
+
+const tarea = ['Hector', 35, true, miCumple, miLibroFavorito ]
+
+console.log(tarea)
